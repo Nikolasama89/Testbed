74e40ae5b0391f091fa492f6c022eb823ad2b79a
Nikos <niqos89@gmail.com>
2024-11-23 14:59:29 +0200

    Hello World created

---
+++ src/gr/aueb/cf/ch1/HelloWorld.java
@@ -0,0 +1,16 @@
+package gr.aueb.cf.ch1;
+
+/**
+ * Prints the phrase hello world!
+ *
+ */
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello Coding Factory");
+        System.out.println();
+    }
+
+}
