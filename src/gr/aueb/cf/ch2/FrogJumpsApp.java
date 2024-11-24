c983f795613cf9a4dad6523386463b85fcacf687
Nikos <niqos89@gmail.com>
2024-11-24 23:14:39 +0200

    class challenge

---
+++ src/gr/aueb/cf/ch2/FrogJumpsApp.java
@@ -0,0 +1,17 @@
+package gr.aueb.cf.ch2;
+
+public class FrogJumpsApp {
+
+    public static void main(String[] args) {
+        int target = 85;
+        int start = 10;
+        int hop = 30;
+        int jumps ;
+
+        jumps = (int)Math.ceil((target - start) / (double)hop);
+
+
+
+        System.out.println("Jumps : " + jumps);
+    }
+}
