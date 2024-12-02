15269825a66cdf73fdddf46206bbd664b5701611
Nikos <niqos89@gmail.com>
2024-12-02 14:55:54 +0200

    class demos

---
+++ src/gr/aueb/cf/ch7/StrCopy.java
@@ -0,0 +1,16 @@
+package gr.aueb.cf.ch7;
+
+/**
+ * Τα strings ειναι immutable και
+ * δεν επηρεαζει το αντιγραφο της αναφορας.
+ * Οποτε εχουμε εγκυρο copy με αντιγραφη της αναφορας.
+ */
+
+public class StrCopy {
+
+    public static void main(String[] args) {
+        String s1 = "Athens";
+        String S2 = s1;
+
+    }
+}
