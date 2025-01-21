d629f483dd80c751252cd7d587920f54547337e4
Nikos <niqos89@gmail.com>
2025-01-21 21:02:56 +0200

    class demos

--- src/gr/aueb/cf/ch17/knight/IKnight.java
+++ src/gr/aueb/cf/ch17/knight/IKnight.java
@@ -3,5 +3,12 @@ package gr.aueb.cf.ch17.knight;
 @FunctionalInterface
 public interface IKnight {
 
+    IMission[] missions = {
+        new KillTheDragonMission(),
+            new SaveThePrincessMission(),
+            new KillTheDragonMission()
+    };
+
+
     void embarkOnMission(IMission mission);
 }
