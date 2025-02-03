505d5cdf7471f07302286c94cbbb5b0de0de8c09
Nikos <niqos89@gmail.com>
2025-02-03 21:02:30 +0200

    class demos

--- src/gr/aueb/cf/ch19/singly_linked_list/SingleList.java
+++ src/gr/aueb/cf/ch19/singly_linked_list/SingleList.java
@@ -15,5 +15,96 @@ public class SingleList<T> {
         head = tmp;
     }
 
+    /**
+     * Time complexity O(n)
+     * @param t the value to be inserted.
+     */
+    public void insertLast(T t) {
+        Node<T> tmp = new Node<>();
+        tmp.setItem(t);
+        tmp.setNext(null);
 
+        if (head == null) {
+            insertFirst(t);
+            return;
+        }
+
+        Node<T> n;
+        for (n = head; n.getNext() != null; n =  n.getNext()) {
+
+        }
+        n.setNext(tmp);
+    }
+
+    /**
+     * Time complexity O(1)
+     * @return
+     */
+    public Node<T> removeFirst() {
+        if (head == null) return null;
+
+        Node<T> node = head;
+        head = head.getNext();
+        return node;
+    }
+
+    /**
+     * Time complexity O(n)
+     * @return the removed node.
+     */
+    public Node<T> removeLast() {
+        if (head == null || head.getNext() == null) {
+            return removeFirst();
+        }
+
+        Node<T> n;
+        for (n = head; n.getNext().getNext() != null; n = n.getNext()) {
+
+        }
+
+        Node<T> nodeToReturn = n.getNext();
+        n.setNext(null);
+        return nodeToReturn;
+    }
+
+    /**
+     * Time-complexity O(n)
+     * @param t the value to search for.
+     * @return the node that contains the value,
+     *          null otherwise.
+     */
+    public Node<T> get(T t) {
+        Node<T> nodeToReturn = null;
+
+        for (Node<T> n = head; n != null; n = n.getNext()) {
+            if (n.getItem().equals(t)) {
+                nodeToReturn = n;
+                break;
+            }
+        }
+        return nodeToReturn;
+    }
+
+    /**
+     * Time-complexity O(n)
+     */
+    public void traverse() {
+        for (Node<T> n =head; n != null; n = n.getNext()) {
+            System.out.println(n.getItem());
+        }
+    }
+
+    // Ωπου εχουμε ελεγχο head == null μπορουμε να αντικασταστησουμε με την isEmpty.
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public int size() {
+        int counter = 0;
+
+        for (Node<T> n = head; n != null; n = n.getNext()) {
+            counter++;
+        }
+        return counter;
+    }
 }
